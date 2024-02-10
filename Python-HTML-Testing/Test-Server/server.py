d7f48ccea07db93561cfa8da016ecd048049f53f
Mark Mueller <muemark10@gmail.com>
2024-02-10 15:54:32 -0600

    #2 more work on local server

--- Python-HTML-Testing/Test-Server/server.py
+++ Python-HTML-Testing/Test-Server/server.py
@@ -3,13 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.php')
 
-@app.route('/my-link/')
-def my_link():
-  print ('I got clicked!')
+# @app.route('/my-link/')
+# def my_link():
+#   print ('I got clicked!')
 
-  return 'Click.'
+#   return 'Click.'
 
 if __name__ == '__main__':
   app.run(debug=True)
