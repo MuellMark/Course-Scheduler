ba3f31af36fc3dcf1db9d6625262b3437163c7c1
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-13 19:31:18 -0600

    course abbreviations and 4 hour check

--- HTML-Templates/testScript.py
+++ HTML-Templates/testScript.py
@@ -1,7 +1,11 @@
 import sys
 
-def main(arg1, arg2):
-    print("Chosen courseID " + arg1 + " /Given course name " + arg2)
+def main(arg1, arg2, arg3, arg4):
+    print("Chosen courseID: " + arg1)
+    print("Given course name: " + arg2)
+    print("Given course abbreviation: " + arg3)
+    if(arg4 == "on"):
+        print("this course meets for 4 hours " + arg4)
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2],sys.argv[3], sys.argv[4])
