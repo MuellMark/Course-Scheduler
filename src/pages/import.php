46383b1f4987b16f92e9a1353c9ee4032f44c0ca
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-19 11:50:26 -0600

    Attempt at editing CSV

--- src/pages/import.php
+++ src/pages/import.php
@@ -2,6 +2,7 @@
     <head>
         <title>CSV Test Viewer</title>
     </head>
+    <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
     <body>
         <?php
             if(isset($_FILES["csv_file"])){
@@ -9,6 +10,11 @@
                 $file = $_FILES["csv_file"];
                 // Source: https://stackoverflow.com/questions/37008227/what-is-the-difference-between-name-and-tmp-name
                 $csvData = file_get_contents($file["tmp_name"]);
+                //if(isset($_REQUEST["newCourse"])){ 
+                    $fileAccess = new SplFileObject($file["tmp_name"], 'a');
+                    $fileAccess->fputcsv(array('4', 'test'));
+                    $fileAccess = null;
+                //} 
                 // Seperate by new line
                 $csvRows = explode("\n", $csvData);
                 foreach ($csvRows as $row) {
@@ -19,12 +25,15 @@
                         echo "$item, ";
                         }
                     }
+                    echo "<br>The file temp location is " .$file["tmp_name"];
             }
         ?>
         <form action="" method="post" enctype="multipart/form-data"><br>
             <label for="csv_file">Upload CSV File:</label><br>
             <input type="file" name="csv_file" id="csv_file" accept=".csv" required><br>
             <button type="submit">View CSV</button>
+            <label for="newCourse">Or Add a New Course:</label><br>
+            <input type="text" id="newCourse" name="newCourse" placeholder="Enter New Course" value="<?=empty($_REQUEST["newCourse"]) ? "" : $_REQUEST["newCourse"] ?>">
         </form>
             <a href="base.php">
             <button>Back</button>
