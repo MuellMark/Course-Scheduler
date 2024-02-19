479200d6df358057d61f11640d6e358d2732e5aa
Mark Mueller <muemark10@gmail.com>
2024-02-18 19:23:42 -0600

    #22 Outline for file_convertor.py
    I'm adding functionality for the command line params

--- Python-Code/File_Convertor.py
+++ Python-Code/File_Convertor.py
@@ -1,5 +1,17 @@
+import sys
+
 from PyGLPK_Solver import *
 
 #Needs command line support, should also return the 
 #The correctly generated script
+
+def one_csv_param():
+    print("TODO")
+
+def two_csv_param():
+    print("TODO")
+# Will only run if file is called directly
+if __name__=="__main__":
+    num_args = len(sys.argv)
+    
 
