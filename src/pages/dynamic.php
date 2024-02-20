f9c7b2472b98b1e8b5e99ba12fb6b13dc71dac18
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-20 00:02:00 -0600

    Added download button and script

--- src/pages/dynamic.php
+++ src/pages/dynamic.php
@@ -7,7 +7,7 @@
 
     <h2>Dynamic Table</h2>
     <button onclick="addRow()">Add Row</button>
-    <button >Save as CSV</button>
+    <button onclick="tableToCSV()">Save as CSV</button>
 
     <table id="dynamic-table">
         <tr>
@@ -46,6 +46,68 @@
             cell8.innerHTML = "<input type='checkbox' id='thursday' name='thursday' value='thursday'>";
             cell9.innerHTML = "<input type='checkbox' id='friday' name='friday' value='friday'>";
         }
+        // https://www.geeksforgeeks.org/how-to-export-html-table-to-csv-using-javascript/
+        // Taken from this website but will be modified to fit our tasks
+        function tableToCSV() {
+ 
+            // Variable to store the final csv data
+            let csv_data = [];
+ 
+            // Get each row data
+            let rows = document.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+ 
+                // Get each column data
+                let cols = rows[i].querySelectorAll('td,th');
+ 
+                // Stores each csv row data
+                let csvrow = [];
+                for (let j = 0; j < cols.length; j++) {
+ 
+                    // Get the text data of each cell
+                    // of a row and push it to csvrow
+                    csvrow.push(cols[j].innerHTML);
+                }
+ 
+                // Combine each column value with comma
+                csv_data.push(csvrow.join(","));
+            }
+ 
+            // Combine each row data with new line character
+            csv_data = csv_data.join('\n');
+ 
+            // Call this function to download csv file  
+            downloadCSVFile(csv_data);
+ 
+        }
+ 
+        function downloadCSVFile(csv_data) {
+ 
+            // Create CSV file object and feed
+            // our csv_data into it
+            CSVFile = new Blob([csv_data], {
+                type: "text/csv"
+            });
+ 
+            // Create to temporary link to initiate
+            // download process
+            let temp_link = document.createElement('a');
+ 
+            // Download csv file
+            temp_link.download = "GfG.csv";
+            let url = window.URL.createObjectURL(CSVFile);
+            temp_link.href = url;
+ 
+            // This link should not be displayed
+            temp_link.style.display = "none";
+            document.body.appendChild(temp_link);
+ 
+            // Automatically click the link to
+            // trigger download
+            temp_link.click();
+            document.body.removeChild(temp_link);
+        }
+    </script>
     </script>
 
     </body>
