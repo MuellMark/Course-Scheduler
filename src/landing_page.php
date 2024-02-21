8e1c4e96d7a99c1ddcfeb1a1c197533321a5cecf
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-21 11:53:48 -0600

    Attempt to connect database #23

--- src/landing_page.php
+++ src/landing_page.php
@@ -35,6 +35,10 @@
 <div id="headerimage">
 <img src="./images/48430_211016_HomecomingDroneSunset-HDR_2 (1).jpg" alt="Picture Of Campus At Sunset">
 </div>
+<form action="view_csv.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="csv" value="" required/>
+        <input type="submit" name="submit" value="View" />
+</form>
 </header>
 
 <h1> Create Course Schedule </h1>
