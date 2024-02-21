fab7e6614785362d7b59f052eb4c11f578318cf9
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-20 18:49:44 -0600

    Renamed and mapped all pages

--- src/pages/landing_page.php
+++ src/pages/landing_page.php
@@ -21,15 +21,15 @@
       </div>
       <div class="UploadPreviouslyCreatedSchedule" style="left: 124px; top: 663px; position: absolute; color: black; font-size: 36px; font-family: Inter; font-weight: 400; word-wrap: break-word">Upload Previously Created Schedule:</div>
       <img class="Screenshot20240203At6133" style="width: 178px; height: 47px; left: 756px; top: 663px; position: absolute" src="https://via.placeholder.com/178x47" />
-      <div class="TodoAddABriefDescriptionAsToWhatThisSiteIsAndWhatItDoes" style="width: 909px; height: 207px; left: 301px; top: 213px; position: absolute; color: black; font-size: 36px; font-family: Inter; font-weight: 400; word-wrap: break-word">TODO: Add a brief description as to what this site is and what it does<br/></div>
+      <div class="TodoAddABriefDescriptionAsToWhatThisSiteIsAndWhatItDoes" style="width: 909px; height: 207px; left: 301px; top: 213px; position: absolute; color: black; font-size: 36px; font-family: Inter; font-weight: 400; word-wrap: break-word">TODO: Add a brief description as to what this site is and what it does<br/>And replace this fantastic looking site for the one with color</div>
       <div class="Rectangle3" style="width: 171px; height: 56px; left: 597px; top: 810px; position: absolute; background: #D9D9D9"></div>
       <div class="Rectangle4" style="width: 171px; height: 56px; left: 841px; top: 810px; position: absolute; background: #D9D9D9"></div>
       <div class="Faq" style="left: 648px; top: 816px; position: absolute; color: black; font-size: 36px; font-family: Inter; font-weight: 400; word-wrap: break-word">FAQ</div>
       <div class="Rectangle5" style="width: 171px; height: 56px; left: 597px; top: 810px; position: absolute; background: #D9D9D9"></div>
       <div class="Faq" style="left: 648px; top: 816px; position: absolute; color: black; font-size: 36px; font-family: Inter; font-weight: 400; word-wrap: break-word">FAQ</div>
       <div class="Rectangle6" style="width: 259px; height: 56px; left: 902px; top: 475px; position: absolute; background: #D9D9D9">
-        <a href="main.php">
-            <button>To Restrictions</button>
+        <a href="dynamic_class_csv.php">
+            <button>Create new csv</button>
         </a>
       </div>
       <div class="Rectangle7" style="width: 176px; height: 56px; left: 995px; top: 657px; position: absolute; background: #D9D9D9"></div>
@@ -37,22 +37,6 @@
       <div class="Github" style="left: 869px; top: 816px; position: absolute; color: black; font-size: 36px; font-family: Inter; font-weight: 400; word-wrap: break-word">Github</div>
       <div class="Or" style="left: 514px; top: 592px; position: absolute; color: black; font-size: 36px; font-family: Inter; font-weight: 400; word-wrap: break-word">Or</div>
     </div>
-    <?php
-            // Can access imported files from the _FILES array
-            $file = $_FILES["csv_file"];
-            // Source: https://stackoverflow.com/questions/37008227/what-is-the-difference-between-name-and-tmp-name
-            $csvData = file_get_contents($file["tmp_name"]);
-            // Seperate by new line
-            $csvRows = explode("\n", $csvData);
-            foreach ($csvRows as $row) {
-                // Source: https://stackoverflow.com/questions/6169235/str-getcsv-example
-                $csvItem = str_getcsv($row);
-                echo "<br>";
-                foreach ($csvItem as $item) {
-                    echo "$item, ";
-                }
-                }
-        ?>
         <form action="" method="post" enctype="multipart/form-data"><br>
             <label for="csv_file">Upload CSV File:</label><br>
             <input type="file" name="csv_file" id="csv_file" accept=".csv" required><br>
