55a230575a71af1d179e17dfbce7f65b64f8f61e
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-22 21:24:59 -0600

    CSV file download working

--- src/createCSV.js
+++ src/createCSV.js
@@ -7,11 +7,11 @@ function tableToCSV() {
 
     // Get each row data
     let rows = document.getElementsByTagName('tr');
-    for (let i = 0; i < rows.length; i++) {
+    for (let i = 1; i < rows.length; i++) {
 
         // Get each column data
         //var test = document.querySelectorAll('input[value][type="checkbox"]:not([value=""])');
-        let cols = rows[i].querySelectorAll('[name="CourseID"]:not([value="empty"],[type="number"],[type="text"]:not([value=""]),input[type="checkbox"]');
+        let cols = rows[i].querySelectorAll('input[type="select"]:not([value="empty"]),input[type="number"],input[type="text"]:not([value=""]),input[type="checkbox"]:checked,[name="meeting_hours"]');
 
         // Stores each csv row data
         let csvrow = [];
