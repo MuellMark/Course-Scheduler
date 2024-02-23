cbb706ae1ac5d90b5f64bbffb62f7bebf0b4f626
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-23 15:06:27 -0600

    Add column to row

--- src/dynamic_faculty_csv.php
+++ src/dynamic_faculty_csv.php
@@ -4,20 +4,18 @@
     <title>Add Course Information Form</title>
     <script type="text/javascript" src="createCSV.js"></script>
 </head>
-    <body>
+<body>
 
     <h2>Dynamic Table</h2>
     <button onclick="addRow()">Add Row</button>
-    <button onclick="addCourse()">Add Course Row</button>
     <button onclick="tableToCSV()">Save as CSV</button>
     <br><a href="final_schedule_result.php">
-            <button>Run script</button>
+        <button>Run script</button>
     </a>
     This doesn't actually run anything yet
     <br><a href="landing_page.php">
-            <button>Back</button>
+        <button>Back</button>
     </a>
-
 
     <table id="dynamic-table">
         <tr>
@@ -37,38 +35,23 @@
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
 
-            /**
-             * TODO There might be an easy way to implement this into the select row
-             * so that it dynamically fills it. A way to do it is create a new 
-             * string that will be concatenated and then placed in cell1.innerHTML
-             */
-            // <label for="courseList">Select a Course:</label>
-            // <select id="courseList" name="courseList">
-            //     <!-- TODO implement a way to dynamically populate dropdown menu from previous responses -->
-            //     <option value="test">Choose one</option>
-            //     <?php 
-            //         $file = fopen("csv/courses.csv", "r");
-
-            //         while (($data = fgetcsv($file)) !== FALSE)
-            //             echo "<option value=\"" .$data[1]. "\">" .$data[1]. "</option>"
-            //     ?>
-            // </select>
             cell1.innerHTML = "<input type='text' id='facultyName' name='facultyName' placeholder='Enter Faculty Name'>";
             cell2.innerHTML = "<select name='primetime'>" +
                                 "<option value='no'>No</option>" +
                                 "<option value='yes'>Yes</option>" +
                                 "</select>";
-            // TODO find a better way to input more courses
             cell3.innerHTML = "<input type='text' id='courses' name='courses' placeholder='Enter Courses Taught'>";
+            // https://www.w3schools.com/jsref/prop_node_parentnode.asp
+            cell4.innerHTML = "<button onclick='addColumn(this.parentNode.parentNode)'>Add Column</button>";
         }
-        function addCourse() {
-            var table = document.getElementById("dynamic-table");
-            var row = table.insertRow(table.rows.length);
-            var cell4 = row.insertCell(4);
-            cell4.innerHTML = "<input type='text' id='courses' name='courses' placeholder='Enter Courses Taught'>";
+
+        function addColumn(row) {
+            var cell = row.insertCell(row.cells.length - 1); // Insert before the last cell (actions cell)
+            cell.innerHTML = "<input type='text' name='courses' placeholder='Enter Courses Taught'>"; // TODO might need an ID
         }
     </script>
 
-    </body>
+</body>
 </html>
