9a6db32bb465b7060d3bd5c96ea64cdb6e53de4f
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-25 19:57:35 -0600

    Organized class restriction

--- src/dynamic_class_csv.php
+++ src/dynamic_class_csv.php
@@ -20,7 +20,7 @@
 
     <table id="dynamic-table">
         <tr>
-            <th>Remove</th>
+            <th>Remove row</th>
             <th>Class name</th>
             <th>Abbreviation</th>
             <th>4 Contact Hours</th>
@@ -36,16 +36,37 @@
         function addRow() {
             var table = document.getElementById("dynamic-table");
             var row = table.insertRow(table.rows.length);
-            var cellDelete = row.insertCell(0);
+            // Remove 	Class name 	Abbreviation 	4 Contact Hours 	Sections 	Unavailable Times 	Select CourseID
+            var cellRemove = row.insertCell(0);
+            cellRemove.innerHTML = '<button type="button" onclick="deleteRow(this)">-</button>';
             var cell2 = row.insertCell(1);
+            cell2.innerHTML = "<input type='text' id='newCourse' name='newCourse' placeholder='Enter New Course'>";
             var cell3 = row.insertCell(2);
+            cell3.innerHTML = "<input type='text' id='abbreviation' name='abbreviation'>";
             var cell4 = row.insertCell(3);
+            cell4.innerHTML = "<select name='meeting_hours' id='meeting'>" +
+                                "<option value='FALSE'>No</option>" +
+                                "<option value='TRUE'>Yes</option>" +
+                                "</select>";
             var cell5 = row.insertCell(4);
+            cell5.innerHTML = "<input type='number' id='sections' name='sections' min='1'>";
             var cell6 = row.insertCell(5);
-            var cell1 = row.insertCell(6);
+            cell6.innerHTML = "<input type='checkbox' id='monday' name='monday' value='monday'>"+
+                                "<input type='checkbox' id='tuesday' name='tuesday' value='tuesday'>"+
+                                "<input type='checkbox' id='wednesday' name='wednesday' value='wednesday'>"+
+                                "<input type='checkbox' id='thursday' name='thursday' value='thursday'>"+
+                                "<input type='checkbox' id='friday' name='friday' value='friday'>";
+            var cell7 = row.insertCell(6);
+            cell7.innerHTML = "<select name='CourseID' id='CourseID'>" +
+                            "<option value='empty'>Choose one</option>" +
+                            "<option value='CS11'>CS11</option>" +
+                            "<option value='CS21'>CS21</option>" +
+                            "<option value='DIS1'>DIS1</option>" +
+                          "</select>";
             var cellAdd = row.insertCell(7);
-
-
+            cellAdd.innerHTML = "<button onclick='addColumn(this.parentNode.parentNode)'>Add Column</button>";
+            // TODO Need to implement a remove course column button
+            
             /**
              * TODO There might be an easy way to implement this into the select row
              * so that it dynamically fills it. A way to do it is create a new 
@@ -62,32 +83,11 @@
             //             echo "<option value=\"" .$data[1]. "\">" .$data[1]. "</option>"
             //     ?>
             // </select>
-            cell1.innerHTML = "<select name='CourseID' id='CourseID'>" +
-                            "<option value='empty'>Choose one</option>" +
-                            "<option value='CS11'>CS11</option>" +
-                            "<option value='CS21'>CS21</option>" +
-                            "<option value='DIS1'>DIS1</option>" +
-                          "</select>";
-            cell2.innerHTML = "<input type='text' id='newCourse' name='newCourse' placeholder='Enter New Course'>";
-            cell3.innerHTML = "<input type='text' id='abbreviation' name='abbreviation'>";
-            cell4.innerHTML = "<select name='meeting_hours' id='meeting'>" +
-                                "<option value='FALSE'>No</option>" +
-                                "<option value='TRUE'>Yes</option>" +
-                                "</select>";
-            cell5.innerHTML = "<input type='number' id='sections' name='sections' min='1'>";
-            // TODO these are unreadable as to what each one is
-            cell6.innerHTML = "<input type='checkbox' id='monday' name='monday' value='monday'>"+
-                                "<input type='checkbox' id='tuesday' name='tuesday' value='tuesday'>"+
-                                "<input type='checkbox' id='wednesday' name='wednesday' value='wednesday'>"+
-                                "<input type='checkbox' id='thursday' name='thursday' value='thursday'>"+
-                                "<input type='checkbox' id='friday' name='friday' value='friday'>";
-            cellDelete.innerHTML = '<button type="button" onclick="deleteRow(this)">-</button>';
-            cellAdd.innerHTML = "<button onclick='addColumn(this.parentNode.parentNode)'>Add Column</button>";
             
         }
         function addColumn(row) {
             var cell = row.insertCell(row.cells.length - 1); // Insert before the last cell (actions cell)
-            cell.innerHTML = "<input type='text' name='courses' placeholder='Enter Courses Taught'>"; // TODO might need an ID
+            cell.innerHTML = "<input type='text' name='courses' placeholder='Enter new course'>"; // TODO might need an ID
         }
         function deleteRow(thisRow) {
             var row = thisRow.parentNode.parentNode;
