a3d3103691fccd91c643f6d56114ac508ccc1c7b
Colby Sullivan <sullivanc@southwestern.edu>
2024-03-21 14:24:09 -0500

    Experimenting with labels

--- src/dynamic_merge.php
+++ src/dynamic_merge.php
@@ -152,16 +152,16 @@
                 // TODO need to fix this and make it match the script
                 var cell6 = row.insertCell(5);
                 // <label for=\"m930\"></label>
-                cell6.innerHTML = "<div>MWF <input type='checkbox' id='m800' name='m800' value='m800'>" +
-                    "<input type='checkbox' id='m930' name='m930' value='m930'>" +
-                    "<input type='checkbox' id='m1100' name='m1100' value='m1100'>" +
-                    "<input type='checkbox' id='m200' name='m200' value='m200'>" +
-                    "<input type='checkbox' id='m330' name='m330' value='m330'></div><div>TTh" +
-                    "<input type='checkbox' id='t830' name='t830' value='t830'>" +
-                    "<input type='checkbox' id='t1000' name='t1000' value='t1000'>" +
-                    "<input type='checkbox' id='t1130' name='t1130' value='t1130'>" +
-                    "<input type='checkbox' id='t100' name='t100' value='t100'>" +
-                    "<input type='checkbox' id='t230' name='t230' value='t230'></div>";
+                cell6.innerHTML = "<fieldset><div>MWF <label for='m800'><input type='checkbox' id='m800' name='m800' value='m800'></label>" +
+                    "<label for='m930'><input type='checkbox' id='m930' name='m930' value='m930'></label>" +
+                    "<label for='m1100'><input type='checkbox' id='m1100' name='m1100' value='m1100'></label>" +
+                    "<label for='m200'><input type='checkbox' id='m200' name='m200' value='m200'></label>" +
+                    "<label for='m330'><input type='checkbox' id='m330' name='m330' value='m330'></label></div><div>TTh" +
+                    "<label for='t830'><input type='checkbox' id='t830' name='t830' value='t830'></label>" +
+                    "<label for='t1000'><input type='checkbox' id='t1000' name='t1000' value='t1000'></label>" +
+                    "<label for='t1130'><input type='checkbox' id='t1130' name='t1130' value='t1130'></label>" +
+                    "<label for='t100'><input type='checkbox' id='t100' name='t100' value='t100'></label>" +
+                    "<label for='t230'><input type='checkbox' id='t230' name='t230' value='t230'></label></div></fieldset>";
 
                 // CourseID
                 var cell7 = row.insertCell(6);
