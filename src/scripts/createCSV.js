f1c0359b4dc9a334b6979fd84180649f568b5916
Colby Sullivan <sullivanc@southwestern.edu>
2024-03-30 18:05:57 -0500

    Reverted csv script

--- src/scripts/createCSV.js
+++ src/scripts/createCSV.js
@@ -11,7 +11,7 @@ function tableToCSV() {
 
     // Get each row data
     let rows = document.getElementsByTagName('tr');
-    for (let i = 2; i < rows.length; i++) {
+    for (let i = 1; i < rows.length; i++) {
 
         // Get each column data
         let cols = rows[i].querySelectorAll('input[name="newCourse"][type="select"]:not([value="empty"]),input[name="sections"][type="number"],input[type="text"]:not([value=""]),input[type="checkbox"]:checked,[name="meeting_hours"],input[name="facultyName"],input[name="courses"],[name="primetime"]');
