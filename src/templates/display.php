b66550e40052850ff92bd1d9617b88ef83e480b0
Hamdally <88158644+Hamdally@users.noreply.github.com>
2024-04-07 15:15:39 -0500

    Pushing Changes

--- src/templates/display.php
+++ src/templates/display.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Test</title>
 </head>
+
 <body>
     <h1><span>Non variable</span></h1>
     <h1><span>csv_data: {{csv_data}}</span></h1>
     <h1><span>Name: {{test}}</span></h1>
 </body>
+
 </html>
 
 <body>
     <h1>CSV Data</h1>
     <table border="1">
         {% for row in csv_data %}
-            <tr>
-                {% for col in row %}
-                    <td>{{ col }}</td>
-                {% endfor %}
-            </tr>
+        <tr>
+            {% for col in row %}
+            <td>{{ col }}</td>
+            {% endfor %}
+        </tr>
         {% endfor %}
     </table>
 </body>
+
 </html>
