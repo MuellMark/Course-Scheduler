b3ff74300f987cce7c4598c13e0f7c1601c47120
Hamdally <88158644+Hamdally@users.noreply.github.com>
2024-04-17 09:58:24 -0500

    Pushing Changes To Template

--- src/templates/display.php
+++ src/templates/display.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
 <html>
 
+
 <head>
-    <title>Optimal Schedule</title>
+    <title>Create CSV</title>
+    <script src="{{url_for('static', filename='scripts/createCSV.js')}}"></script>
+    <script type="text/javascript" src="scripts/faculty_script.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/3.7.4/firebase.js"></script>
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='css/style.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-    <h1>Optimal Schedule</h1>
+    <header>
+        <div id="headerBar">
+            <div class="hamburger" onclick="toggleMenu()"> &#9776;</div>
+            <!--Logo Placement-->
+            <div id="logo">
+                <img src="{{url_for('static', filename='images/Logo.png')}}">
+            </div>
+            <!--Navigation Bar-->
+            <nav>
+                <div id="menubar">
+                    <ul>
+                        <li> <a href="{{ url_for('home')}}"> Home</a> </li>
+                        <li> <a href="{{ url_for('faq')}}"> FAQ</a> </li>
+                        <li> <a href="{{ url_for('option')}}"> Create Schedule</a> </li>
+                        <li> <a href="{{ url_for('howto')}}"> How To Guides</a> </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <!--Header Pic-->
+        <div id="headerImage">
+            <img src="{{url_for('static', filename='images/48430_211016_HomecomingDroneSunset-HDR_2 (1).jpg')}}">
+        </div>
+    </header>
+<body>
+    <h1>Course Schedule</h1>
     <table border="1">
         <th>Time</th>
         <th>CourseID</th>
