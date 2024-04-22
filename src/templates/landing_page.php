e663e16de00d7c3a53839283bef396fbccbcf0cc
Colby Sullivan <sullivanc@southwestern.edu>
2024-04-21 21:24:58 -0500

    Removed celebration

--- src/templates/landing_page.php
+++ src/templates/landing_page.php
@@ -61,32 +61,11 @@
 
     <!-- JavaScript used to enable hamburger menu -->
     <script>
-        const start = () => {
-            setTimeout(function () {
-                confetti.start()
-            }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
-        };
-
-        //  for stopping the confetti 
-
-        const stop = () => {
-            setTimeout(function () {
-                confetti.stop()
-            }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
-        };
-        // after this here we are calling both the function so it works
-        start();
-        stop();
-
         window.onload = function () { //When webpage opens, run this code
             var menu = document.getElementById('menubar');
             if (window.innerWidth < 750) { //If the windows width is less than 750 px, then hide the menu
                 menu.style.display = 'none'
             }
-
-            start();
-            stop();
-            playSound();
         };
 
         function toggleMenu() {
@@ -100,12 +79,6 @@
                 contentWrapper.classList.add('menu-opened');
             }
         }
-
-        function playSound() {
-            const audio = new Audio("{{url_for('static', filename='confetti.mp3')}}"); // Replace 'path_to_your_sound_file.mp3' with the actual path to your sound file
-            audio.play();
-        }
-        window.onload = playSound;
     </script>
 </body>
 </div>
