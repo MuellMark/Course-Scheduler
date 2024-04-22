a8e8474c1eccfc96c577f6c9141ce5937f976bc2
Colby Sullivan <sullivanc@southwestern.edu>
2024-04-21 20:46:36 -0500

    Database issues are working

--- src/templates/display.php
+++ src/templates/display.php
@@ -57,7 +57,7 @@
 
     <table id="optimalTable">
         <th>Time</th>
-        <th>Course ID</th>
+        <th>Course Abbreviation</th>
         <th>Faculty</th>
         {% for row in csv_data %}
         <tr>
