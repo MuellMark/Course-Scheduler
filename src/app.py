3b70d28ffff5c63ede3647c98a5725593fa7fbbc
Mark Mueller <muemark10@gmail.com>
2024-04-28 20:02:39 -0500

    #272 revert infeasible changes

--- src/app.py
+++ src/app.py
@@ -123,7 +123,7 @@ def force():
         csv_function_data = getCSVData()
         return render_template('display.php', csv_data=csv_function_data)
     else:
-        return render_template('display.php', csv_data=csv_function_data)
+        return "Not Feasible"
 
 @app.route("/swap", methods=['GET', 'POST'])
 def swap():
@@ -142,7 +142,7 @@ def swap():
         csv_function_data = getCSVData()
         return render_template('display.php', csv_data=csv_function_data)
     else:
-        return render_template('display.php', csv_data=csv_function_data)
+        return "Not Feasible"
 
 def notInfeasible():
     with open('user_output.csv', 'r', newline='') as file:
