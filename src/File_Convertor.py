d3028d0ec565a246617ae8b61abf7fbc94ce0787
Mark Mueller <muemark10@gmail.com>
2024-04-29 03:45:12 -0500

    Auto stash before revert of "Revert "#272 webapge clean up and comments""

--- src/File_Convertor.py
+++ src/File_Convertor.py
@@ -302,3 +302,5 @@ if __name__=="__main__":
         # two_csv_param(sys.argv[1],sys.argv[2])
     elif(num_args==5):
         one_csv_param(sys.argv[1],sys.argv[2],sys.argv[4])
+    else:
+        print("error, too many parameters")
